6c62ab89b3a5e28db5ffb6e3086248e17c6abcf5
Vando Pinto <vandopintoo@gmail.com>
2026-05-30 11:13:36 -0300

    ajustes

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,18 +1,5 @@
-
-// function atualizarOrientacao() {
-
-//     if (window.innerHeight > window.innerWidth) {
-//         $("#status").html("📱 Modo Retrato");
-//     } else {
-//         $("#status").html("📺 Modo Paisagem");
-//     }
-
-// }
-
 $("#playButton").on("click", async function () {
-
     try {
-
         // Entrar em fullscreen
         if (document.documentElement.requestFullscreen) {
             await document.documentElement.requestFullscreen();
@@ -22,34 +9,14 @@ $("#playButton").on("click", async function () {
         if (screen.orientation && screen.orientation.lock) {
 
             try {
-                await screen.orientation.lock("portrait");
-
-                $("#status").html(
-                    "✅ Fullscreen ativado<br>🔒 Orientação travada em Retrato"
-                );
-
-            } catch (e) {
-
-                $("#status").html(
-                    "✅ Fullscreen ativado<br>⚠️ Seu navegador não permite travar a orientação"
-                );
-
+                await screen.orientation.lock("landscape");
+            } catch (err) {
+                console.warn("Não foi possível bloquear a orientação:", err);
             }
-
         }
-
     } catch (err) {
-
         $("#status").html(
             "❌ Fullscreen não permitido"
         );
-
     }
-
 });
-
-// $(window).on("resize orientationchange", function () {
-//     atualizarOrientacao();
-// });
-
-// atualizarOrientacao();
